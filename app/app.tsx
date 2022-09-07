f5c23d53246090f8d82a0467ec0b0ba31161e539
Tomas Baronas <tomas.r.baronas@gmail.com>
2022-09-07 15:13:16 +0200

    feat: add theme and remove unecessary App.tsx

--- app/app.tsx
+++ app/app.tsx
@@ -1,13 +1,26 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import store from './configureStore';
 import Navigator from './navigator';
 
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
+};
+
 export default function App() {
   return (
     <Provider store={store}>
-      <Navigator />
+      <PaperProvider theme={theme}>
+        <Navigator />
+      </PaperProvider>
     </Provider>
   );
 }
