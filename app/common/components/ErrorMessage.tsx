50f80149a646698d4e5def9ee295c4eff907fcc5
Tomas Baronas <tomas.r.baronas@gmail.com>
2022-09-21 14:00:34 +0200

    fix(app): fix linting on ErrorMessage

--- app/common/components/ErrorMessage.tsx
+++ app/common/components/ErrorMessage.tsx
@@ -2,7 +2,11 @@ import * as React from 'react';
 import {Text} from 'react-native-paper';
 
 const ErrorMessage = (props: {error: Error}) => {
-  return <Text style={{color: 'red', marginTop: 20}}>Error: {props.error.message}</Text>;
+  return (
+    <Text style={{color: 'red', marginTop: 20}}>
+      Error: {props.error.message}
+    </Text>
+  );
 };
 
 export default ErrorMessage;
